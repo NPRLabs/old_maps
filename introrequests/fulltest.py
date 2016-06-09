82be38818e996b7a1f91d9af19ed9df9c0982efb
Augustus Wynn <wyna@seas.upenn.edu>
2016-06-09 11:02:42 -0400

    first put together, now trying to test downloads

--- introrequests/fulltest.py
+++ introrequests/fulltest.py
@@ -1,52 +1,88 @@
 import argparse
-
-
-
-parser = argparse.ArgumentParser(description='small tool')
-
-
-parser.add_argument('which', choices=['tv', 'am', 'fm'])
-
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-r', '--reload_from_source', action='store_true')
-group.add_argument('-f', '--load_file', default='', dest='filename')
-
-parser.add_argument('-c', '--call_sign', default='', dest='call_sign')
 import requests
 import time
 
-r = requests.get('https://api.github.com/events')
 
-payload = {'call':'',
-'arn':'',
-'state':'',
-'city':'',
-'freq':'0.0',
-'fre2':'107.9',
-'serv':'',
-'vac':'',
-'facid':'',
-'asrn':'',
-'class':'',
-'list':'3',
-'ThisTab':'Results+to+This+Page%2FTab',
-'dist':'',
-'dlat2':'',
-'mlat2':'',
-'slat2':'',
-'NS':'N',
-'dlon2':'',
-'mlon2':'',
-'slon2':'',
-'EW':'W',
-'size':9}
+def setup_args():
+    parser = argparse.ArgumentParser(description='small tool')
 
-chunk_size = 1024
-r = requests.get('https://transition.fcc.gov/fcc-bin/fmq', params=payload, stream=True)
-print r.url
-for line in r.iter_lines(chunk_size):
-  #  print (' '.join(line.split())).replace(' ', ',')
-  if line: 
-    for entry in filter(lambda l: 'KSFH' in l, line.split(' ')):
-      if entry:
-        print entry
+
+    parser.add_argument('which', choices=['tv', 'am', 'fm'])
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-r', '--reload_from_source', action='store_true')
+    group.add_argument('-f', '--load_file', default='', dest='filename')
+
+    parser.add_argument('-c', '--call_sign', default='', dest='call_sign')
+    return parser
+
+def load_from_website(args, filename):
+    stdout = filename and filename != '-'
+    f = None
+    if not stdout:
+        f = open(filename, 'wb')
+
+    r = requests.get('https://api.github.com/events')
+
+    payload = {'call':'',
+    'arn':'',
+    'state':'',
+    'city':'',
+    'serv':'',
+    'vac':'',
+    'facid':'',
+    'asrn':'',
+    'class':'',
+    'list':'3',
+    'ThisTab':'Results+to+This+Page%2FTab',
+    'dist':'',
+    'dlat2':'',
+    'mlat2':'',
+    'slat2':'',
+    'NS':'N',
+    'dlon2':'',
+    'mlon2':'',
+    'slon2':'',
+    'EW':'W',
+    'size':9}
+
+    
+    if args.which == 'tv':
+        payload['chan'] = '0.0'
+        payload['cha2'] = '69'
+    elif args.which == 'fm':
+        payload['freq'] = '0.0'
+        payload['fre2'] = '107.9'
+    else:
+        payload['freq'] = '530'
+        payload['fre2'] = '1700'
+
+
+    url = 'https://transition.fcc.gov/fcc-bin/{}q'.format(args.which)
+    print 'Url to use:{}'.format(url)
+    chunk_size = 1024
+    r = requests.get(url, params=payload, stream=True)
+    print r.url
+    for line in r.iter_lines(chunk_size):
+        #  print (' '.join(line.split())).replace(' ', ',')
+        if line: 
+            if stdout:
+                print line
+            else:
+                f.write(line)
+
+    if not stdout:
+        f.close()
+
+
+
+if __name__ == '__main__':
+    parser = setup_args()
+    args = parser.parse_args()
+    if args.reload_from_source:
+        load_from_website(args, args.filename)
+
+  
+
+
+
