e350fba709c0b2969e96699675ba1fa469c91244
Augustus Wynn <wyna@seas.upenn.edu>
2016-06-14 09:52:11 -0400

    fixed fill.py api (still need to think about uniqueness and also make adding and upating api, as well as write documentation. also fixed problem with argpase + copy.py

--- sqlstart/count.py
+++ sqlstart/count.py
@@ -1,12 +1,12 @@
 import sqlite3
-
+import sys
 
 db = sqlite3.connect('fcc.db')
 
 
 c = db.cursor()
 
-c.execute("SELECT COUNT(*) FROM tv")
+c.execute("SELECT COUNT(*) FROM {}".format(sys.argv[1]))
 print c.fetchall()
 '''for row in c.execute("SELECT * FROM tv"):
     print row
--- sqlstart/fill.py
+++ sqlstart/fill.py
@@ -1,43 +1,11 @@
+import argparse
+
 import sqlite3
 import sys
 import random
-import argparse
-import requests
 import time
 import sys
 import math
-'''
-fm_defaults = { 'callsign' : None, 'da' : 'ND', 'channel' : None, 'class' : None,
-                'service' : None, 'freq' : None, 'status' : None, 'city' : None, 
-                'state' : None, 'country' : None, 'fn' : None, 'fid' : None, 
-                'erph' : None, 'erpv' : None, 'haath' : None, 'haatv' : None,
-                'lat' : None, 'long' : None, 'name' : None, 'rcamlslh' : None,
-                'rcamlslv' : None, 'daid' : None, 'dapr' : None, 'asrn' : None,
-                'h' : None, 'appid' : None, 'dmi' : None, 'dkm' : None, 'ddeg' : None, 'org' : None }
-
-am_defaults = { 'callsign' : None, 'da' : 'ND', 'usclass' : None, 'iclass' : None,
-                'service' : None, 'hours' : None, 'freq' : None, 'status' : None, 'city' : None, 
-                'state' : None, 'country' : None, 'fn' : None, 'fid' : None, 
-                'power' : None,'lat' : None, 'long' : None, 'name' : None,
-                'appid' : None, 'dmi' : None, 'dkm' : None, 'ddeg' : None, 'org' : None }
-
-tv_defaults = { 'callsign' : None, 'da' : 'ND', 'channel' : None, 'tvzone' : None, 'tvstatus' : None,
-                'service' : None, 'freqoff' : None, 'status' : None, 'city' : None, 
-                'state' : None, 'country' : None, 'fn' : None, 'fid' : None, 
-                'erp' : None, 'haat' : None, 'lat' : None, 'long' : None, 'name' : None, 
-                'rcamlsl' : None, 'polar' : None, 'daid' : None, 'dapr' : None, 'asrn' : None,
-                'h' : None, 'appid' : None, 'dmi' : None, 'dkm' : None, 'ddeg' : None, 
-                'virtchan' : None,'org' : None }
-'''
-def execmany():
-    db = sqlite3.connect('test.db')
-    c = db.cursor()
-
-    #c.executemany('''INSERT INTO execmany(name, phone, email, password) VALUES(?,?,?,?)''',rand_vals())
-    
-    db.commit()
-    db.close()
-
 
 def setup_args():
     parser = argparse.ArgumentParser(description='small tool')
@@ -45,7 +13,8 @@ def setup_args():
 
     parser.add_argument('which', choices=['tv', 'am', 'fm'])
     parser.add_argument('-o', '--output-file', default='', dest='outfile')
-
+    parser.add_argument('-s', '--load_file', default='', dest='filename')
+    '''
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-r', '--reload_from_source', action='store_true')
     group.add_argument('-s', '--load_file', default='', dest='filename')
@@ -53,16 +22,17 @@ def setup_args():
     group2 = parser.add_mutually_exclusive_group()
     group2.add_argument('-q', '--query', nargs=2, default='', dest='query')
     group2.add_argument('-l', '--listquery', nargs=2, default='', dest='listquery')
-    group2.add_argument('-f', '--filter', help='1:field filter by 2: value it must be, 3:new filename', nargs=3, default='', dest='filt')
-
+    group2.add_argument('-f', '--filter', 
+            help='1:field filter by 2: value it must be, 3:new filename', 
+                nargs=3, default='', dest='filt')
+    '''
     return parser
 
 def format_line(line):
-    
-        vals = line.split('|')
-        if not vals[0]:
-            vals.pop(0)
-        return ','.join(map(str.strip,vals))
+    vals = line.split('|')
+    if not vals[0]:
+        vals.pop(0)
+    return ','.join(map(str.strip,vals))
 
 
 def lat_to_real(hem, deg, minutes, secs):
@@ -120,7 +90,6 @@ def line_read(line, typ):
                 output.append(None)
             elif i in [13,14,15,16,27,28,29,30,35]:
                 e = entry.split()
-                print e
                 if entry == '' or e[0] == '-' :
                     output.append(None)
                 else:
@@ -136,70 +105,60 @@ def line_read(line, typ):
         output.append(None)
     #deal with org
     o = tuple(output)
-    print o
     return o
 
-fm_sql ='''INSERT INTO fm VALUES({})'''.format('?,'*30 + '?')
-am_sql = '''INSERT INTO am VALUES({})'''.format('?,'*21 + '?')
+fm_sql = '''INSERT OR IGNORE INTO fm VALUES({})'''.format('?,'*30 + '?')
+am_sql = '''INSERT OR IGNORE INTO am VALUES({})'''.format('?,'*21 + '?')
 tv_sql = '''INSERT OR IGNORE INTO tv VALUES({})'''.format('?,'*29 + '?')
 
-def insert_list(l, sql):
-    db = sqlite3.connect('fcc.db')
+def insert_list(db_f, l, sql):
+    db = sqlite3.connect(db_f)
     c = db.cursor()
-   # for e in l:
-    #    print e
-    #    c.execute(sql,e)
+
     c.executemany(sql,
             l)
     db.commit()
     db.close()
 
 
-def query_file(filename, value, option):
+def fill_file(filename, out, which):
     f = open(filename, 'r')
-    query_func = None
-    if option == 'callsign':
-        query_func = call_q
-    elif option == 'freq':
-        query_func = freq_q
+
+    sql = None
+    if which == 'fm':
+        sql = fm_sql
+    elif which == 'fm':
+        sql = am_sql
+    elif which == 'tv':
+        sql = tv_sql
 
     list_to_insert = []
-    print list_to_insert
     for i, line in enumerate(f):
         if line:
-            list_to_insert.append(line_read(line, 'tv'))
+            list_to_insert.append(line_read(line, which))
             if i % 10000 == 0 and i > 0:
-                insert_list(list_to_insert, tv_sql)
+                print 'Attempting to insert up to {}'.format(i)
+                insert_list(out, list_to_insert, sql)
                 list_to_insert = []
             
-    insert_list(list_to_insert, tv_sql)
+    # fill last ones
+    insert_list(out, list_to_insert, sql)
     f.close()
 
 if __name__ == '__main__':
     parser = setup_args()
     args = parser.parse_args()
-    if args.reload_from_source:
-        load_from_website(args, args.outfile, args.callsign)
-    filename = None
+    outfile = None
+    if args.outfile:
+        outfile = args.outfile
+    else:
+        outfile = 'fcc.db'
     if args.filename:
         filename = args.filename
     else:
         filename = 'data/{}_data.txt'.format(args.which)
-    if args.query:
-        print 'good'
-        query_file(filename, args.query[1],args.query[0])
-    elif args.listquery:
-        fl = open(args.listquery[1], 'r')
-        print args.listquery
-        for line in fl:
-            print 'AAA{}AAA'.format(line)
-            print 'Query for: {}'.format(line)
-            query_file(filename, line.rstrip(), args.listquery[0])
-            print 
-    elif args.filt:
-        print args.filt
-        filter_by(filename, args.filt[2], args.filt[1], args.filt[0]) 
 
+    fill_file(filename, outfile, args.which)
   
 
 
