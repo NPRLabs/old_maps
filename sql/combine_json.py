f137a0c5015893eecb0839a33c21bf11f5b9989c
Augustus Wynn <wyna@seas.upenn.edu>
2016-07-05 13:40:47 -0400

    filtered duplicates and impoved list comprehension skills

--- sql/combine_json.py
+++ sql/combine_json.py
@@ -9,6 +9,10 @@ import json
 
 
 
+#def check_loc(f, d):
+    #return f[
+def load_json(d):
+    return json.loads(d[0])['features']
 
 if __name__ == '__main__':
     db = sqlite3.connect('fcc.db')
@@ -16,10 +20,37 @@ if __name__ == '__main__':
 
     cur.execute('''SELECT con FROM fm WHERE con NOT NULL''')
 
-    features_list = [feat for con in cur for feat in json.loads(con[0])['features']] 
+    # most likely need to optimize
+    features_list = [
+        {
+            "geometry":{"type":"GeometryCollection",
+                        "geometries": [ con[0]['geometry'],
+                                        con[1]['geometry'] ]},
+                        "type":"Feature",
+                        "properties":con[0]["properties"],
+                        "id":i+1}
+                        for i,con in enumerate( map(load_json, cur) )] 
+    #print len(features_list)
+    #print features_list[0]['geometry']
+    #features_list = [item for item in features_list if 'geometries' in item['geometry']]
+    #print len(features_list)
+
+    lats = []
+    filtered_list = []
+    for f in features_list:
+        lat = f["geometry"]["geometries"][0]["coordinates"]
+        if not lat in lats:
+            lats.append(lat)
+            filtered_list.append(f)
+
+    
+    #print "Features: " + str(len(features_list))
+    #print "Filtered: " + str(len(filtered_list))
+
+
 
     d = {"type":"FeatureCollection"}
-    d['features'] = features_list
+    d['features'] = filtered_list
     print 'var test_json = '
     print json.dumps(d)
 
