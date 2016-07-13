ec7eca3f38adfdb8dee2977461cdbf8cc02300eb
Augustus Wynn <wyna@seas.upenn.edu>
2016-07-13 11:55:48 -0400

    renamed the main js file, and tried to change getting the favicon, but reverted to the new version

--- flask_testing/first_app.py
+++ flask_testing/first_app.py
@@ -13,6 +13,10 @@ if app.debug:
 # sqlite3 database
 DATABASE = 'fcc.db'
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_file('static/favicon.ico')
+
 # make the database accessible by get_db()
 def get_db():
     db = getattr(g, '_database', None)
@@ -59,6 +63,7 @@ def give_json():
             and lat < ?
             ''', (w, s, e, n), 35)
         return jsonify(**d)
+        
         
         
 @app.teardown_appcontext
